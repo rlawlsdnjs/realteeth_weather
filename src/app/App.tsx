d38ed26f3fac213f667e3542b2d4fd7019d5d8e2
jinwonkim <dlfjs9514@nate.com>
2026-01-15 22:48:05 +0900

    feat: CI/CD 설정 추가 및 앱 배포 테스트

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 export function App() {
   return (
     <div className="p-6 text-2xl font-bold text-red-600">
-      <h1>RealTeeth Weather 🌦️</h1>
+      <h1>RealTeeth Weather 🌦️ vercel CI </h1>
     </div>
   );
 }
