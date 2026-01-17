121c115af64503cd249164b0c1d2267bba1f228e
jinwonkim <dlfjs9514@nate.com>
2026-01-18 01:11:18 +0900

    feat : 라우터 설정

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { SearchPage } from "../pages/SearchPage";
+import { LocationDetailPage } from "../pages/LocationDetailPage";
+import { FavoritesPage } from "../pages/FavoritesPage";
+
 export function App() {
   return (
-    <div className="p-6 text-2xl font-bold text-red-600">
-      <h1>RealTeeth Weather 🌦️ vercel CI </h1>
-    </div>
+    <BrowserRouter>
+      <div className="h-screen w-screen overflow-hidden">
+        <Routes>
+          <Route path="/" element={<SearchPage />} />
+          <Route path="/location/:id" element={<LocationDetailPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
